3dfc408dcd1b4b2369388ab6fa7270e4e2dea22e
Allan Klaus Do Nascimento Bezerra <allan_klaus@globo.com>
2016-02-07 18:48:07 -0200

    Done test to destroy recipe again

--- spec/features/user/user_destroy_recipe_spec.rb
+++ spec/features/user/user_destroy_recipe_spec.rb
@@ -4,24 +4,16 @@ feature 'User destroy his recipe' do
   scenario 'successfully' do
     user = login
     recipe = create_list(:recipe, 5, user: user)
-    visit destroy_recipe_path(recipe[0])
+    visit my_recipes_path
 
-    expect(page).to_not have_content recipe[0].naem
+    click_on recipe[0]
+
+    expect(page).to_not have_content recipe[0].name
   end
 
   scenario 'user unlogger' do
     recipe = create(:recipe)
-    visit destroy_recipe_path(recipe)
+    visit my_recipes_path
     expect(page).to have_content I18n.t('generic.login')
-  end
-
-  scenario "unsuccessfully, user try to edit a job that don't belong to him" do
-    user = login
-
-    recipe = create(:recipe)
-
-    visit destroy_recipe_path(recipe)
-
-    expect(current_path).to eq my_recipes_path
   end
 end
