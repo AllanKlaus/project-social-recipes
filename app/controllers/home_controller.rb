2f9d302caa1dce082847a10c842abb00b56a611e
Allan Klaus Do Nascimento Bezerra <allan_klaus@globo.com>
2016-02-07 16:54:28 -0200

    User now see home organized

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,9 @@ class HomeController < ApplicationController
   def index
     @recipes = Recipe.last(NUMBER_TO_LIST)
     @recipes_favorites = Recipe.order(favorite: :desc).limit(NUMBER_TO_LIST)
+
+    @kitchens = Kitchen.all.order(name: :asc)
+    @food_types = FoodType.all.order(name: :asc)
+    @preferences = Preference.all.order(name: :asc)
   end
 end
