0a7e27b0d059a3989e3aa14357dab87ec22c87d1
Allan Klaus Do Nascimento Bezerra <allan_klaus@globo.com>
2016-02-09 11:27:19 -0200

    Created tests to user send mail

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'User send mail' do
+  context 'user send recipe' do
+    it 'send a recipe' do
+      user = create(:user)
+      info = { mail: 'allan_klaus@globo.com' }
+
+      expect { user.send_recipe(self, info) }.to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+  end
 end
