2e0fd1bf1ebe235f9edbaf039e59bea8d6b2a4c0
Allan Klaus Do Nascimento Bezerra <allan_klaus@globo.com>
2016-02-10 17:09:35 -0200

    Created test to user send his city on registration

--- spec/features/user/user_create_account_with_name_spec.rb
+++ spec/features/user/user_create_account_with_name_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-feature 'User create account with his name' do
-  scenario 'successfully' do
+feature 'User create account' do
+  scenario 'successfully sending name' do
     user = build(:user)
 
     visit new_user_registration_path
@@ -13,5 +13,26 @@ feature 'User create account with his name' do
 
     click_on 'submit'
     expect(User.last.name).to eq user.name
+  end
+    scenario 'unsuccessfully' do
+      visit new_user_registration_path
+
+      click_on 'submit'
+      expect(page).to have_content I18n.t('errors.messages.blank')
+    end
+
+  scenario 'successfully sending city' do
+    user = build(:user, city: FFaker::Name.name)
+
+    visit new_user_registration_path
+
+    fill_in 'user[name]',                   with: user.name
+    fill_in 'user[email]',                  with: user.email
+    fill_in 'user[password]',               with: user.password
+    fill_in 'user[password_confirmation]',  with: user.password
+    fill_in 'user[city]',                   with: user.city
+
+    click_on 'submit'
+    expect(User.last.city).to eq user.city
   end
 end
