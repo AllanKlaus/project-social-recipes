839cf8ae03f49aace08df4603f6e43061a6e54e7
Allan Klaus Do Nascimento Bezerra <allan_klaus@globo.com>
2016-02-10 17:10:53 -0200

    User now can register himself including his city

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,7 +12,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :city)
   end
 
   def account_update_params
