e6ffd88a14254b37f0dea3eff5520ec0ed00add5
Allan Klaus Do Nascimento Bezerra <allan_klaus@globo.com>
2016-02-10 19:19:12 -0200

    Created tests to user see links on site and the link direct user to recipe path

---
+++ spec/features/user/user_see_links_to_recipe_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+feature 'User see links to recipe' do
+  scenario 'on home' do
+    recipe = create(:recipe)
+
+    visit root_path
+
+    click_on recipe.name
+
+    expect(current_path).to eq recipe_path(recipe)
+  end
+
+  scenario 'on kitchens path' do
+    kitchen = create(:kitchen)
+    recipe = create(:recipe, kitchen: kitchen)
+
+    visit kitchen_path(kitchen)
+
+    click_on recipe.name
+
+    expect(current_path).to eq recipe_path(recipe)
+  end
+
+  scenario 'on preferences path' do
+    preference = create(:preference)
+    recipe = create(:recipe, preference: preference)
+
+    visit preference_path(preference)
+
+    click_on recipe.name
+
+    expect(current_path).to eq recipe_path(recipe)
+  end
+
+  scenario 'on food types path' do
+    food_type = create(:food_type)
+    recipe = create(:recipe, food_type: food_type)
+
+    visit food_type_path(food_type)
+
+    click_on recipe.name
+
+    expect(current_path).to eq recipe_path(recipe)
+  end
+
+  scenario 'on my recipes path' do
+    user = login
+    recipe = create(:recipe, user: user)
+
+    visit my_recipes_path
+
+    click_on recipe.name
+
+    expect(current_path).to eq recipe_path(recipe)
+  end
+
+  scenario 'on my favorites recipes path' do
+    user = login
+
+    recipe = create(:recipe)
+    visit recipe_path(recipe)
+    click_on I18n.t('generic.add_favorite_list')
+
+    visit favorite_recipes_path
+
+    click_on recipe.name
+
+    expect(current_path).to eq recipe_path(recipe)
+  end
+end
