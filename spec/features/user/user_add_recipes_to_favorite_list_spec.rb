62f3a432f6e05d8792d0ad7c76a01bfe1a7bbae3
Allan Klaus Do Nascimento Bezerra <allan_klaus@globo.com>
2016-02-11 22:59:31 -0200

    Created test to dont permit user add one recipe twice

--- spec/features/user/user_add_recipes_to_favorite_list_spec.rb
+++ spec/features/user/user_add_recipes_to_favorite_list_spec.rb
@@ -19,4 +19,18 @@ feature 'User add a recipe to his favorite list' do
     visit recipe_path(recipe)
     expect(page).to_not have_content I18n.t('generic.add_favorite_list')
   end
+
+  scenario 'cant add a recipe twice' do
+    user = login
+
+    recipe = create(:recipe)
+    visit recipe_path(recipe)
+    click_on I18n.t('generic.add_favorite_list')
+    favorites_count = user.favorites.count
+
+    visit recipe_path(recipe)
+    click_on I18n.t('generic.add_favorite_list')
+
+    expect(favorites_count).to eq user.favorites.count
+  end
 end
